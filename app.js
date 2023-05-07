f57677f35167863946d71b9b923774735351061a
shailesh <shaileshkryadav13@gmail.com>
2023-05-07 10:20:19 +0545

    stage 1 auth done

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+const user = mongoose.model("user", { email: String, password: String });
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -17,6 +21,36 @@ app.get("/login", (req, res) => {
 
 app.get("/register", (req, res) => {
   res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+  try {
+    const newUser = new user({ email: email, password: password });
+    newUser.save();
+    res.render("secrets");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    let checkUser = await user.find({
+      email: req.body.username,
+      password: req.body.password,
+    });
+    console.log(checkUser);
+    if (checkUser.length !== 0) {
+      res.render("secrets");
+    } else {
+      console.log("user not found");
+      res.redirect("/register");
+    }
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.listen(3000, console.log("The server is running at port 3000"));
